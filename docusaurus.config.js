e26e4c949499594b26a70599c9591ae7b11ce438
vascoalexander <vascoalexander@gmail.com>
2025-01-31 18:49:39 +0100

    title and description changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'My Documentation',
+  tagline: 'Coding is Cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
