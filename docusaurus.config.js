2fa33156b9ff5009f4c04bebb553cc8482c917af
vascoalexander <vascoalexander@gmail.com>
2025-01-31 19:03:24 +0100

    editurl fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/vascoalexander/my-documentation/tree/main/docs/',
+            'https://github.com/vascoalexander/my-documentation/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -58,7 +58,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/vascoalexander/my-documentation/tree/main/blog/',
+            'https://github.com/vascoalexander/my-documentation/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
